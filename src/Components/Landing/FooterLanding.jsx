15f97770fb947a062a6894de91f542376d46eaac
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-06-11 12:27:50 +0600

    2

--- src/Components/Landing/FooterLanding.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FooterLanding() {
-  return (
-    <div>FooterLanding</div>
-  )
-}
--- src/Components/Landing/HeaderLanding.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HeaderLanding() {
-  return (
-    <div>HeaderLanding</div>
-  )
-}
