817c50f340a9efc622c6f921b8237ca592672387
Sulaimanova Nadira <nadira18.sulaimanova@gmail.com>
2022-06-12 00:27:57 +0600

    add new style

--- src/Components/Posts/Communities/CommunitiesPage.jsx
+++ src/Components/Posts/Communities/CommunitiesPage.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../Posts.css'
+import logo from '../../../image/logo.png'
+import user from '../../../image/user.png'
+import userPhoto from '../../../image/userphoto.png'
+import Communities from '../Communities/CommunitiesCategories'
+import comunitiesPhoto from '../../../image/communitiesphoto.png'
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 
 export default function CommunitiesPage() {
+  const [communities, setCommunities] = useState('')
+
+    const handleClick = () => {
+        setCommunities(!communities)
+    }
   return (
-    <div>CommunitiesPage</div>
+    <div>
+      <div className='row '>
+            <div className='panel'>
+                <a href="#"><img src={logo} alt="" /></a>
+                <a href="#" className='panel-icon'><img src={user} alt="" /></a>
+            </div>
+            <div className='user-panel'>
+                <img src={userPhoto} alt="" />
+                <p>Манас Султанов</p>
+                {communities &&
+                    <div className='user-panel-button' onClick={handleClick}>
+                        <div>
+                            Сообщества
+                        </div>
+                        <span>
+                            <BiChevronDown />
+                        </span>
+                    </div>
+                }
+                {!communities && <div className='user-panel-button' onClick={handleClick}>
+                    <div>
+                        Сообщества
+                    </div>
+                    <span>
+                        <BiChevronUp />
+                    </span>
+                </div>}
+                {communities && <div className='communities'>
+                    <Communities />
+                </div>}
+            </div>
+            <div className='communities-container'>
+                <img src={comunitiesPhoto} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
