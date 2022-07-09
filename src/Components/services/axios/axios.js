38da094cf7b167895782c57e4823ad906bd3e473
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-07-09 14:37:08 +0600

    js

---
+++ src/Components/services/axios/axios.js
@@ -0,0 +1,20 @@
+import { configure } from "@testing-library/react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const axiosInstance = axios.create({
+    baseURL: "htt/api",
+});
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const authToken = Cookies.get("auth-token");
+
+        if (authToken) {
+            config.headers.authorization = `Bearer ${authToken}`;
+        }
+        return config
+    }, 
+    (error) => Promise.reject(error)
+);
+
+export default axiosInstance
